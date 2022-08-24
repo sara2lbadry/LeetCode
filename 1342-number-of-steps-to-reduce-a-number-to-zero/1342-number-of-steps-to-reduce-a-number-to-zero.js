b6f746b286d3e39ed03e7dbe35071306c02dfad4
Sara El Badry <58861008+sara2lbadry@users.noreply.github.com>
2022-08-24 18:03:53 +0200

    Time: 95 ms (39.64%), Space: 42.1 MB (34.48%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    var steps = 0
+    while(num !== 0) {
+        if(num % 2 === 0)
+            num /= 2
+        else 
+            num -= 1
+        steps += 1   
+    }
+    return steps
+    
+};

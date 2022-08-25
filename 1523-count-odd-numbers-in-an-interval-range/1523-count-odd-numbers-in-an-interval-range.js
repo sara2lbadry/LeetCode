506ffaefc612aaebc0511907920ef89a7c913c65
Sara El Badry <58861008+sara2lbadry@users.noreply.github.com>
2022-08-25 13:11:20 +0200

    Time: 4229 ms (42.27%), Space: 41.2 MB (99.42%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function(low, high) {
+    var count = 0
+    for(var i = low; i <= high; i++) {
+        if(i % 2 !== 0)
+            count++
+    }
+    
+    return count
+    
+};
